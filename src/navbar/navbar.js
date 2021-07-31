ef23482a77d487d283fbb8ec0feea8eeb78dcdd6
debanjan <debanjan.goswami@webskitters.com>
2021-07-31 13:21:15 +0530

    re-check

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -197,7 +197,7 @@ const handleChange=(e)=>{
               })}
             </ul>
           <div className="clear-text">
-          <button>clear All</button>
+          <button>Clear</button>
           </div>
           
             </div>
