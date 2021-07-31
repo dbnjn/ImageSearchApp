227204c6cad85db64916620078809a5a2d0b028a
debanjan <debanjan.goswami@webskitters.com>
2021-07-31 12:41:34 +0530

    again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React,{useState} from "react";
-import './App.css';
+
 import ImageDisplayComp from "./ImgComp";
 
 import Navbar from "./navbar/navbar"
--- src/ImgComp/index.js
+++ src/ImgComp/index.js
@@ -1,6 +1,5 @@
 
 import React,{useState,useEffect} from "react"
-import "./index.css";
 import axios from "axios";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import {BaseUrl,API_KEY} from "../apis";
